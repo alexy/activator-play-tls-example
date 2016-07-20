9a829a78ed9dc1ebba6034df954afac1da8dbc50
Alexy Khrabrov <deliverable@gmail.com>
2016-07-20 00:52:51 -0400

    try client's cipher preference

--- app/https/CustomSSLEngineProvider.scala
+++ app/https/CustomSSLEngineProvider.scala
@@ -79,7 +79,7 @@ class CustomSSLEngineProvider(appProvider: ApplicationProvider) extends SSLEngin
 
     // Tells the server to ignore client's cipher suite preference.
     // http://docs.oracle.com/javase/8/docs/technotes/guides/security/jsse/JSSERefGuide.html#cipher_suite_preference
-    sslParameters.setUseCipherSuitesOrder(true)
+    ////sslParameters.setUseCipherSuitesOrder(true)
 
     // http://docs.oracle.com/javase/8/docs/technotes/guides/security/jsse/JSSERefGuide.html#SSLParameters
     val needClientAuth = java.lang.System.getProperty("play.ssl.needClientAuth")
